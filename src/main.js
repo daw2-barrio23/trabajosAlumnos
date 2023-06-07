1df82ce948e429ebfafe5a5eff6510b1ccb33174
daw2-barrio23 <barriomunoznicolas@fpllefia.com>
2023-06-07 18:46:03 +0200

    prueba?

--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@ import * as bootstrap from 'bootstrap'
 import { home } from "./vistas/home";
 import {header} from "./componentes/header"
 import {footer} from "./componentes/footer"
+import { Perfil } from './bd/perfil';
 
 document.querySelector('main').innerHTML = home.templeate;
 document.querySelector('header').innerHTML = header.templeate;
 document.querySelector('footer').innerHTML = footer.templeate;
 
+console.log(Perfil.getAll());
